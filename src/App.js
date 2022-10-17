82d5825346f940346fe7a3bda65452adf14c91fb
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-10-17 10:20:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import NavBar from './components/layout/Navbar'
+import './index.css';
 
+import './App.css';
+import Index from './components/Index';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <Index />
+      
+     
+    </>
   );
 }
 
